a2fb14c88de31a4f67b1fafea1b2b3e8de8bd757
Raul França <raulmfranca@gmail.com>
2022-11-05 03:18:48 -0300

    add htmls

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+)
+
+type Rsvp struct { // RSVP responda por favor
+	Name, Email, Phone string
+	WillAttend         bool
+}
+
+// Vai funcionar como um banco de dados com as respostas
+var responses = make([]*Rsvp, 0, 10)                   // []slice, make(type, len inicial, capacidade)
+var templates = make(map[string]*template.Template, 3) //map[key]valor, tamanho
+// template do tipo Template
+
+func loadTemplates() {
+	templateNames := [5]string{"welcome", "form", "thanks", "sorry", "list"} //lista dos nomes dos html/template
+	for index, name := range templateNames {
+		//"layout.html", name + ".html" concatena layout com os arquivos. show!!
+		t, err := template.ParseFiles("layout.html", name+".html")
+		if err == nil { // se nao over error add a templates que um map [key] value
+			templates[name] = t // map templates onde key eh name e o conteudo é um Template
+			fmt.Println("Loaded template", index, name)
+		} else {
+			panic(err)
+		}
+	}
+}
 
 func main() {
-
-	fmt.Println("TODO: add algumas features, bora!?")
+	loadTemplates()
 
 }
